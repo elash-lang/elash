2f4efeaf34b5539bc024a0186a162cffd588760c
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-09 19:53:44 +0200

    feat(tests): reorganize test structure by component

--- tests/build.mk
+++ tests/build.mk
@@ -1,24 +1,42 @@
 TESTS_DIR     := tests
 TESTS_OUT_DIR := $(OUT_DIR)/tests
 
-TESTS_SRCS := $(call rwildcard,$(TESTS_DIR),*.c)
-TESTS_BINS := $(patsubst $(TESTS_DIR)/%.c,$(TESTS_OUT_DIR)/%$(EXE_EXT),$(TESTS_SRCS))
+ELASH_TESTS_DIR := $(TESTS_DIR)/elash
+ELC_TESTS_DIR   := $(TESTS_DIR)/elc
 
-TESTS_CFLAGS  := $(CFLAGS)
-TESTS_LDFLAGS := $(LDFLAGS) -lcriterion $(LLVM_LDFLAGS)
+ELASH_TESTS_SRCS := $(call rwildcard,$(ELASH_TESTS_DIR),*.c)
+ELASH_TESTS_BINS := $(patsubst $(TESTS_DIR)/%.c,$(TESTS_OUT_DIR)/%$(EXE_EXT),$(ELASH_TESTS_SRCS))
 
-.PHONY: test-dirs test
+ELC_TESTS_SRCS := $(call rwildcard,$(ELC_TESTS_DIR),*.c)
+ELC_TESTS_BINS := $(patsubst $(TESTS_DIR)/%.c,$(TESTS_OUT_DIR)/%$(EXE_EXT),$(ELC_TESTS_SRCS))
+
+TESTS_CFLAGS := $(CFLAGS)
+TESTS_LDFLAGS := $(LDFLAGS) -lcriterion
+
+.PHONY: test-dirs test test-elash test-elc
 
 test-dirs:
 	@$(call CMD_MKDIR_P,$(TESTS_OUT_DIR))
 
-$(TESTS_OUT_DIR)/%$(EXE_EXT): $(TESTS_DIR)/%.c $(LIBELC_STATIC) $(LIBELASH_STATIC) | test-dirs
+$(TESTS_OUT_DIR)/elash/%$(EXE_EXT): $(TESTS_DIR)/elash/%.c $(LIBELASH_STATIC) | test-dirs
 	@$(call CMD_MKDIR_P,$(dir $@))
-	$(CC) $(TESTS_CFLAGS) $< $(LIBELC_STATIC) $(LIBELASH_STATIC) $(TESTS_LDFLAGS) -o $@
+	$(CC) $(TESTS_CFLAGS) $< $(LIBELASH_STATIC) $(TESTS_LDFLAGS) -o $@
 
-test: $(TESTS_BINS)
-	@$(foreach t,$(TESTS_BINS), \
+$(TESTS_OUT_DIR)/elc/%$(EXE_EXT): $(TESTS_DIR)/elc/%.c $(LIBELC_STATIC) $(LIBELASH_STATIC) | test-dirs
+	@$(call CMD_MKDIR_P,$(dir $@))
+	$(CC) $(TESTS_CFLAGS) $< $(LIBELC_STATIC) $(LIBELASH_STATIC) $(TESTS_LDFLAGS) $(LLVM_LDFLAGS) -o $@
+
+test-elash: $(ELASH_TESTS_BINS)
+	@$(foreach t,$(ELASH_TESTS_BINS), \
 		echo "Running $(patsubst $(TESTS_OUT_DIR)/%,%,$(t))..." && \
 		$(call FIXPATH,$(t)) && \
-	) \
-	echo "All tests passed."
+	) true
+
+test-elc: $(ELC_TESTS_BINS)
+	@$(foreach t,$(ELC_TESTS_BINS), \
+		echo "Running $(patsubst $(TESTS_OUT_DIR)/%,%,$(t))..." && \
+		$(call FIXPATH,$(t)) && \
+	) true
+
+test: test-elash test-elc
+	@echo "All tests passed."
--- tests/elc/test-elc-dummy.c
+++ tests/elc/test-elc-dummy.c
@@ -1,10 +1,10 @@
 // dummy test cases to see if test system works
 #include <criterion/criterion.h>
 
-Test(dummy, pass) {
+Test(elc_dummy, pass) {
     cr_assert(1 == 1, "1 should be equal to 1");
-}
+} 
 
-Test(dummy, fail) {
+Test(elc_dummy, fail) {
     cr_assert(1 == 2, "1 should not be equal to 2");
 }
