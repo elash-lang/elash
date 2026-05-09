352e4d366fd7f1da09f73e0b73cdba3b392638c9
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-09 12:01:42 +0200

    feat(mir/dump): add dump value function

---
+++ include/elash/mir/dump/value.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <elash/mir/value.h>
+#include <stdio.h>
+
+void el_mir_dump_value(const ElMirValue* value, FILE* out);
---
+++ src/elash/mir/dump/value.c
@@ -0,0 +1,33 @@
+#include <elash/mir/dump/value.h>
+#include <elash/sema/symbol/dump.h>
+#include <elash/sema/type.h>
+
+#include <inttypes.h>
+
+void el_mir_dump_value(const ElMirValue* value, FILE* out) {
+    switch (value->kind) {
+    case EL_MIR_VAL_REG:
+        fprintf(out, "%%r%"PRIu32, value->as.reg.id);
+        break;
+    case EL_MIR_VAL_ARG:
+        fprintf(out, "%%a%"PRIu32, value->as.arg.idx);
+        break;
+    case EL_MIR_VAL_CONST:
+        if (value->type->kind == EL_TYPE_PRIM) {
+            switch (value->type->as.prim.kind) {
+            case EL_PRIMTYPE_INT:   fprintf(out, "$%"PRId64, value->as.constant.lit.as.int_);   break;
+            case EL_PRIMTYPE_UINT:  fprintf(out, "$%"PRIu64, value->as.constant.lit.as.uint_);  break;
+            case EL_PRIMTYPE_CHAR:  fprintf(out, "$'%c'", value->as.constant.lit.as.char_);     break;
+            }
+        } else {
+            fputs("<unhandled const>", out);
+        }
+        break;
+    case EL_MIR_VAL_GLOBAL:
+        el_sema_dump_symbol(value->as.global.sym, out);
+        break;
+    }
+
+    fputs(":", out);
+    el_sema_dump_type(value->type, out);
+}
