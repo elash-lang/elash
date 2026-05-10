c5fd7366b14a594b237fcab3d671798d58f4cb9a
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-10 07:54:14 +0200

    refactor(elc/codegen): change LIR emit functions to return memory buffers

--- include/elc/codegen/lir.h
+++ include/elc/codegen/lir.h
@@ -1,12 +1,21 @@
 #pragma once
 
+#include <elash/defs/int-types.h>
 #include <elash/defs/sv.h>
+
+typedef struct ElcCodegenBuffer {
+    uint8_t* data;
+    usize    size;
+} ElcCodegenBuffer;
 
 typedef struct ElcLirHandle ElcLirHandle;
 typedef void ElcLirFreeFn   (ElcLirHandle* lir);
 typedef void ElcLirDumpFn   (const ElcLirHandle* lir, FILE* out);
-typedef void ElcLirEmitAsmFn(const ElcLirHandle* lir, FILE* out);
-typedef void ElcLirEmitObjFn(const ElcLirHandle* lir, FILE* out);
+
+typedef ElcCodegenBuffer ElcLirEmitAsmFn(const ElcLirHandle* lir);
+typedef ElcCodegenBuffer ElcLirEmitObjFn(const ElcLirHandle* lir);
+
+typedef void ElcLirFreeBufferFn(const ElcLirHandle* lir, ElcCodegenBuffer buffer);
 
 typedef struct ElcLirHandle {
     ElStringView ir_name; // for example "llvm ir"
@@ -16,5 +25,6 @@ typedef struct ElcLirHandle {
     ElcLirEmitAsmFn* emit_asm;  // NULL if emitting assembly is unsupported
     ElcLirEmitObjFn* emit_obj;  // always non-null
 
-    ElcLirFreeFn* free; // NULL if no cleanup is needed
+    ElcLirFreeFn* free;               // NULL if no cleanup is needed
+    ElcLirFreeBufferFn* free_buffer;  // NULL if buffers don't need manual freeing
 } ElcLirHandle;
