a093a285253ac8d1500ddc50b689ce1e464bd725
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-10 16:54:27 +0200

    feat(elc/llvm-cg): implement basic function body codegen (WIP)

--- src/elc/codegen/builtin/llvm-backend.c
+++ src/elc/codegen/builtin/llvm-backend.c
@@ -1,6 +1,7 @@
 #include <elc/codegen/builtin/llvm-backend.h>
 
 #include <elash/util/dynarena.h>
+#include <elash/util/assert.h>
 #include <elash/util/todo.h>
 
 #include <llvm-c/Core.h>
@@ -83,6 +84,69 @@ LLVMTypeRef elc_llvm_map_type(BackendContext* ctx, ElType* type) {
     EL_TODO("Unreachable or unhandled type");
 }
 
+static LLVMValueRef elc_llvm_map_value(
+    BackendContext* ctx,
+    LLVMValueRef llvm_func,
+    LLVMValueRef* regs,
+    ElMirValue* value
+) {
+    switch (value->kind) {
+    case EL_MIR_VAL_CONST: {
+        LLVMTypeRef type = elc_llvm_map_type(ctx, value->type);
+        switch (value->type->as.prim.kind) {
+        case EL_PRIMTYPE_INT:
+            return LLVMConstInt(type, value->as.constant.lit.as.int_, true);
+        case EL_PRIMTYPE_UINT:
+            return LLVMConstInt(type, value->as.constant.lit.as.uint_, false);
+        case EL_PRIMTYPE_CHAR:
+            return LLVMConstInt(type, value->as.constant.lit.as.char_, false);
+        }
+        EL_UNREACHABLE_ENUM_VAL(ElPrimitiveTypeKind, value->type->as.prim.kind);
+    }
+    case EL_MIR_VAL_ARG:
+        return LLVMGetParam(llvm_func, value->as.arg.idx);
+    case EL_MIR_VAL_REG:
+        return regs[value->as.reg.id];
+    case EL_MIR_VAL_GLOBAL:
+        EL_TODO("Global values not implemented");
+    }
+    return NULL;
+}
+
+static void elc_llvm_compile_func(BackendContext* ctx, LLVMModuleRef module, ElMirFunc* mir_func) {
+    LLVMTypeRef func_type = elc_llvm_map_type(ctx, mir_func->symbol->as.func.type);
+
+    char* name = el_dynarena_make_cstr(ctx->arena, mir_func->symbol->name);
+    LLVMValueRef llvm_func = LLVMAddFunction(module, name, func_type);
+
+    LLVMValueRef* regs        = EL_DYNARENA_NEW_ARR_ZEROED(ctx->arena, LLVMValueRef, mir_func->reg_count);
+    LLVMBasicBlockRef* blocks = EL_DYNARENA_NEW_ARR_ZEROED(ctx->arena, LLVMBasicBlockRef, mir_func->block_count);
+
+    for (ElMirBlock* mir_block = mir_func->first_block; mir_block != NULL; mir_block = mir_block->next) {
+        blocks[mir_block->id] = LLVMAppendBasicBlockInContext(ctx->context, llvm_func, "block");
+    }
+
+    for (ElMirBlock* mir_block = mir_func->first_block; mir_block != NULL; mir_block = mir_block->next) {
+        LLVMPositionBuilderAtEnd(ctx->builder, blocks[mir_block->id]);
+
+        for (usize i = 0; i < mir_block->instr_count; ++i) {
+            ElMirInstr* instr = mir_block->instructions[i];
+            switch (instr->kind) {
+            case EL_MIR_INSTR_RET: {
+                LLVMValueRef val = NULL;
+                if (instr->as.return_.value != NULL) {
+                    val = elc_llvm_map_value(ctx, llvm_func, regs, instr->as.return_.value);
+                }
+                LLVMBuildRet(ctx->builder, val);
+                break;
+            }
+            default:
+                EL_TODO("implement codegen for all instructions");
+            }
+        }
+    }
+}
+
 static ElcCodegenResult elc_llvm_compile(
     ElcCodegenBackend* self,
     const ElMirModule* input,
@@ -94,10 +158,7 @@ static ElcCodegenResult elc_llvm_compile(
     lir_data->module = LLVMModuleCreateWithNameInContext("elash-module", ctx->context);
 
     for (ElMirFunc* func = input->first_func; func != NULL; func = func->next) {
-        LLVMTypeRef func_type = elc_llvm_map_type(ctx, func->symbol->as.func.type);
-
-        char* name = el_dynarena_make_cstr(ctx->arena, func->symbol->name);
-        LLVMAddFunction(lir_data->module, name, func_type);
+        elc_llvm_compile_func(ctx, lir_data->module, func);
     }
 
     *output = elc_llvm_make_lir_handle(lir_data);
