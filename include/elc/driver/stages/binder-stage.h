be641c4ad1cbec2f75eae29ffc0d455b5cf89666
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-10 19:12:19 +0200

    improve(elc/driver): add missing #pragma once guards to driver stage headers

--- include/elc/driver/stages/binder-stage.h
+++ include/elc/driver/stages/binder-stage.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <elc/pipeline/pipeline.h>
 
 bool elc_binder_stage_exec(const ElcStage* stage, ElcPipelineContext* ctx, const ElcArtifact* input, ElcArtifact* output);
--- include/elc/driver/stages/parser-stage.h
+++ include/elc/driver/stages/parser-stage.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <elc/pipeline/pipeline.h>
 
 bool elc_parser_stage_exec(const ElcStage* stage, ElcPipelineContext* ctx, const ElcArtifact* input, ElcArtifact* output);
