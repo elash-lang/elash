f34359dc59eee875def5d3f2ccfd981d5b743757
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-11 19:33:12 +0200

    fix(hir/dump): add var def dumping

--- src/elash/hir/dump/stmt.c
+++ src/elash/hir/dump/stmt.c
@@ -1,7 +1,11 @@
 #include <elash/hir/dump/stmt.h>
 #include <elash/hir/dump/expr.h>
 #include <elash/hir/dump/indent.h>
+
+#include <elash/sema/type.h>
 #include <elash/hir/tree/stmt.h>
+
+#include <elash/util/assert.h>
 
 void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
     if (!node) return;
@@ -11,7 +15,7 @@ void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
         case EL_HIR_STMT_EXPR:
             el_hir_dump_expr(curr->as.expr, indent, out);
             fputs(";\n", out);
-            break;
+            continue;
 
         case EL_HIR_STMT_RETURN:
             el_hir_dump_print_indent(indent, out);
@@ -21,7 +25,21 @@ void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
                 el_hir_dump_expr(curr->as.return_.value, 0, out);
             }
             fputs(";\n", out);
-            break;
+            continue;
+
+        case EL_HIR_STMT_VAR_DEF: {
+            ElHirVarDefStmtNode* var_def = &curr->as.var_def;
+            el_hir_dump_print_indent(indent, out);
+            el_sema_dump_type(var_def->var->as.var.type, out);
+            fputc(' ', out);
+            el_sv_print(var_def->var->name, out);
+            if (var_def->init != NULL) {
+                fputs(" = ", out);
+                el_hir_dump_expr(var_def->init, 0, out);
+            }
+            fputs(";\n", out);
+            continue;
+        }
 
         case EL_HIR_STMT_BLOCK:
             el_hir_dump_print_indent(indent, out);
@@ -29,7 +47,8 @@ void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
             el_hir_dump_stmt(curr->as.block.stmts, indent + 1, out);
             el_hir_dump_print_indent(indent, out);
             fputs("}\n", out);
-            break;
+            continue;
         }
+        EL_UNREACHABLE_ENUM_VAL(ElHirStmtKind, curr->kind);
     }
 }
