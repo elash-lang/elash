75c704372721eced77feb91b59f9c9d74ee135b8
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-11 19:05:54 +0200

    feat(parser): add variable definition statement parsing

--- src/elash/parser/parse-stmt.c
+++ src/elash/parser/parse-stmt.c
@@ -65,6 +65,30 @@ ElParserErrorCode _el_parser_parse_block(ElParser* parser, ElToken lbrace_tok, E
     return _el_parser_ret_ok(parser);
 }
 
+ElParserErrorCode _el_parser_parse_var_def(ElParser* parser, ElAstStmtNode** out) {
+    ElParserErrorCode result;
+    ElAstTypeNode* type;
+    result = _el_parser_parse_type(parser, &type);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    ElAstIdentNode* name;
+    result = _el_parser_parse_ident(parser, &name);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    ElAstExprNode* init = NULL;
+    if (el_parser_match(parser, EL_TT_ASSIGN)) {
+        result = _el_parser_parse_expression(parser, &init);
+        if (result != EL_PARSER_ERR_OK) return result;
+    }
+
+    ElToken semi_tok = parser->current;
+    result = el_parser_expect(parser, EL_TT_SEMICOLON);
+    if (result != EL_PARSER_ERR_OK) return result;
+
+    *out = el_ast_new_var_def_stmt(parser->arena, el_source_span_merge(type->span, semi_tok.span), type, name, init);
+    return _el_parser_ret_ok(parser);
+}
+
 ElParserErrorCode _el_parser_parse_stmt(ElParser* parser, ElAstStmtNode** out) {
     if (el_parser_check(parser, EL_TT_KW_RETURN)) {
         ElToken return_tok = parser->current;
@@ -76,5 +100,13 @@ ElParserErrorCode _el_parser_parse_stmt(ElParser* parser, ElAstStmtNode** out) {
         el_parser_advance(parser);
         return _el_parser_parse_block(parser, lbrace_tok, out);
     }
+
+    if (el_parser_check(parser, EL_TT_IDENT)) {
+        ElTokenType next_type = el_parser_peek(parser).type;
+        if (next_type == EL_TT_IDENT || next_type == EL_TT_STAR) {
+            return _el_parser_parse_var_def(parser, out);
+        }
+    }
+
     return _el_parser_parse_expr_stmt(parser, out);
 }
