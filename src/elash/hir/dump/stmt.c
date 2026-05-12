5876962bb2d19b3eb290e0190276272eeffb0d39
Maqi-x <186316794+maqi-x@users.noreply.github.com>
2026-05-12 13:58:00 +0200

    refactor(hir/dump): refactor dump stmt function

--- src/elash/hir/dump/stmt.c
+++ src/elash/hir/dump/stmt.c
@@ -8,60 +8,58 @@
 #include <elash/util/assert.h>
 
 void el_hir_dump_stmt(ElHirStmtNode* node, usize indent, FILE* out) {
-    if (!node) return;
+    switch (node->kind) {
+    case EL_HIR_STMT_EXPR:
+        el_hir_dump_expr(node->as.expr, indent, out);
+        fputs(";\n", out);
+        return;
 
-    for (ElHirStmtNode* curr = node; curr; curr = curr->next) {
-        switch (curr->kind) {
-        case EL_HIR_STMT_EXPR:
-            el_hir_dump_expr(curr->as.expr, indent, out);
-            fputs(";\n", out);
-            continue;
-
-        case EL_HIR_STMT_RETURN:
-            el_hir_dump_print_indent(indent, out);
-            fputs("return", out);
-            if (curr->as.return_.value) {
-                fputs(" ", out);
-                el_hir_dump_expr(curr->as.return_.value, 0, out);
-            }
-            fputs(";\n", out);
-            continue;
-
-        case EL_HIR_STMT_VAR_DEF: {
-            ElHirVarDefStmtNode* var_def = &curr->as.var_def;
-            el_hir_dump_print_indent(indent, out);
-            el_sema_dump_type(var_def->var->as.var.type, out);
-            fputc(' ', out);
-            el_sv_print(var_def->var->name, out);
-            if (var_def->init != NULL) {
-                fputs(" = ", out);
-                el_hir_dump_expr(var_def->init, 0, out);
-            }
-            fputs(";\n", out);
-            continue;
+    case EL_HIR_STMT_RETURN:
+        el_hir_dump_print_indent(indent, out);
+        fputs("return", out);
+        if (node->as.return_.value) {
+            fputs(" ", out);
+            el_hir_dump_expr(node->as.return_.value, 0, out);
         }
+        fputs(";\n", out);
+        return;
 
-        case EL_HIR_STMT_IF:
-            el_hir_dump_print_indent(indent, out);
-            fputs("if (", out);
-            el_hir_dump_expr(node->as.if_.cond, 0, out);
-            fputs(")\n", out);
-            el_hir_dump_stmt(node->as.if_.then, indent + 1, out);
-            if (node->as.if_.else_ != NULL) {
-                el_hir_dump_print_indent(indent, out);
-                fputs("else\n", out);
-                el_hir_dump_stmt(node->as.if_.else_, indent + 1, out);
-            }
-            continue;
-
-        case EL_HIR_STMT_BLOCK:
-            el_hir_dump_print_indent(indent, out);
-            fputs("{\n", out);
-            el_hir_dump_stmt(curr->as.block.stmts, indent + 1, out);
-            el_hir_dump_print_indent(indent, out);
-            fputs("}\n", out);
-            continue;
+    case EL_HIR_STMT_VAR_DEF: {
+        ElHirVarDefStmtNode* var_def = &node->as.var_def;
+        el_hir_dump_print_indent(indent, out);
+        el_sema_dump_type(var_def->var->as.var.type, out);
+        fputc(' ', out);
+        el_sv_print(var_def->var->name, out);
+        if (var_def->init != NULL) {
+            fputs(" = ", out);
+            el_hir_dump_expr(var_def->init, 0, out);
         }
-        EL_UNREACHABLE_ENUM_VAL(ElHirStmtKind, curr->kind);
+        fputs(";\n", out);
+        return;
     }
+
+    case EL_HIR_STMT_IF:
+        el_hir_dump_print_indent(indent, out);
+        fputs("if (", out);
+        el_hir_dump_expr(node->as.if_.cond, 0, out);
+        fputs(")\n", out);
+        el_hir_dump_stmt(node->as.if_.then, indent + 1, out);
+        if (node->as.if_.else_ != NULL) {
+            el_hir_dump_print_indent(indent, out);
+            fputs("else\n", out);
+            el_hir_dump_stmt(node->as.if_.else_, indent + 1, out);
+        }
+        return;
+
+    case EL_HIR_STMT_BLOCK:
+        el_hir_dump_print_indent(indent, out);
+        fputs("{\n", out);
+        for (ElHirStmtNode* curr = node->as.block.stmts; curr; curr = curr->next) {
+            el_hir_dump_stmt(curr, indent + 1, out);
+        }
+        el_hir_dump_print_indent(indent, out);
+        fputs("}\n", out);
+        return;
+    }
+    EL_UNREACHABLE_ENUM_VAL(ElHirStmtKind, node->kind);
 }
