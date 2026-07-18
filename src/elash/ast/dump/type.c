dfa7e30fe2a51457a8bd0c6e6c7e3e0f8cf41ef5
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-07-18 20:14:51 +0200

    feat(ast/dump): improve dump type function

--- src/elash/ast/dump/type.c
+++ src/elash/ast/dump/type.c
@@ -2,16 +2,26 @@
 #include <elash/ast/dump/indent.h>
 #include <elash/ast/dump/expr.h>
 
+#include <elash/util/assert.h>
+
 void el_ast_dump_type(ElAstType* node, usize indent, FILE* out) {
     el_ast_dump_print_indent(indent, out);
     switch (node->kind) {
     case EL_AST_TYPE_NAME:
         fprintf(out, "TypeName(\""EL_SV_FMT"\")\n", EL_SV_FARG(node->name->name));
-        break;
+        return;
     case EL_AST_TYPE_REF:
-        fprintf(out, "PointerType:\n");
+        fprintf(out, "ReferenceType:\n");
         el_ast_dump_type(node->base, indent + 1, out);
-        break;
+        return;
+    case EL_AST_TYPE_RAW_SLICE:
+        fprintf(out, "RawSliceType:\n");
+        el_ast_dump_type(node->base, indent + 1, out);
+        return;
+    case EL_AST_TYPE_SLICE:
+        fprintf(out, "SliceType:\n");
+        el_ast_dump_type(node->base, indent + 1, out);
+        return;
     case EL_AST_TYPE_ARRAY:
         fprintf(out, "ArrayType:\n");
         el_ast_dump_print_indent(indent + 1, out);
@@ -20,6 +30,7 @@ void el_ast_dump_type(ElAstType* node, usize indent, FILE* out) {
         el_ast_dump_print_indent(indent + 1, out);
         fprintf(out, "size:\n");
         el_ast_dump_expr(node->array.size, indent + 2, out);
-        break;
+        return;
     }
+    EL_UNREACHABLE_ENUM_VAL(ElAstTypeKind, node->kind);
 }
