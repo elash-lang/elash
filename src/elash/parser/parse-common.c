5db96c7503e36460c92370f5d6fbc2039ed4d088
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-07-19 10:30:18 +0200

    refactor(parser): move lookahead_skip_type to parse-type too

--- src/elash/parser/parse-common.c
+++ src/elash/parser/parse-common.c
@@ -11,27 +11,3 @@ ElAstIdent* _el_parser_parse_ident(ElParser* parser) {
     ElToken tok = el_parser_advance(parser);
     return el_ast_new_ident_raw(parser->arena, tok.span, tok.lexeme);
 }
-
-bool _el_parser_lookahead_skip_type(ElParser* parser, usize* idx) {
-    if (el_parser_peek_at(parser, *idx).type != EL_TT_IDENT) return false;
-    (*idx)++;
-
-    while (true) {
-        ElToken tok = el_parser_peek_at(parser, *idx);
-        if (tok.type == EL_TT_BITWISE_AND) {
-            (*idx)++;
-        } else if (tok.type == EL_TT_LBRACKET) {
-            (*idx)++;
-            int depth = 1;
-            while (depth > 0) {
-                tok = el_parser_peek_at(parser, (*idx)++);
-                if (tok.type == EL_TT_EOF) return false;
-                if (tok.type == EL_TT_LBRACKET) depth++;
-                if (tok.type == EL_TT_RBRACKET) depth--;
-            }
-        } else {
-            break;
-        }
-    }
-    return true;
-}
--- src/elash/parser/parse-type.c
+++ src/elash/parser/parse-type.c
@@ -41,3 +41,27 @@ ElAstType* _el_parser_parse_type(ElParser* parser) {
 
     return type;
 }
+
+bool _el_parser_lookahead_skip_type(ElParser* parser, usize* idx) {
+    if (el_parser_peek_at(parser, *idx).type != EL_TT_IDENT) return false;
+    (*idx)++;
+
+    while (true) {
+        ElToken tok = el_parser_peek_at(parser, *idx);
+        if (tok.type == EL_TT_BITWISE_AND) {
+            (*idx)++;
+        } else if (tok.type == EL_TT_LBRACKET) {
+            (*idx)++;
+            int depth = 1;
+            while (depth > 0) {
+                tok = el_parser_peek_at(parser, (*idx)++);
+                if (tok.type == EL_TT_EOF) return false;
+                if (tok.type == EL_TT_LBRACKET) depth++;
+                if (tok.type == EL_TT_RBRACKET) depth--;
+            }
+        } else {
+            break;
+        }
+    }
+    return true;
+}
